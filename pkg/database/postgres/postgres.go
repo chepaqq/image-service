0dcf210c9f2303fcefdb7cbede5da8dc6675eee7
chepaqq <chepaqq@protonmail.com>
2023-08-20 19:46:57 +0300

    refactor: Pass entirely connection string to postgres connection function

--- pkg/database/postgres/postgres.go
+++ pkg/database/postgres/postgres.go
@@ -1,18 +1,13 @@
-package db
+package postgres
 
 import (
-	"fmt"
 	"time"
 
-	"github.com/chepaqq99/jungle-task/internal/config"
 	"github.com/jmoiron/sqlx"
 )
 
-func ConnectPostgres(cfg config.PostgresConfig) (*sqlx.DB, error) {
-	db, err := sqlx.Open(
-		"postgres",
-		fmt.Sprintf("user=%s dbname=%s host=%s password=%s port=%s sslmode=%s", cfg.User, cfg.DBName, cfg.Host, cfg.Password, cfg.Port, cfg.SSLMode),
-	)
+func ConnectPostgres(url string) (*sqlx.DB, error) {
+	db, err := sqlx.Open("postgres", url)
 	db.SetMaxOpenConns(25)
 	db.SetMaxIdleConns(25)
 	db.SetConnMaxLifetime(5 * time.Minute)
