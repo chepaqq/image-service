c0307c46cff2ca18d7252ba7435c6bdb8b40c774
chepaqq <chepaqq@protonmail.com>
2023-08-21 12:53:54 +0300

    feat: Implement server package

---
+++ pkg/server/server.go
@@ -0,0 +1,63 @@
+package server
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+)
+
+const (
+	defaultReadTimeout     = 5 * time.Second
+	defaultWriteTimeout    = 5 * time.Second
+	defaultShutdownTimeout = 15 * time.Second
+	defaultIdleTimeout     = 1 * time.Minute
+)
+
+// Server represents http server.
+type Server struct {
+	server          *http.Server
+	notify          chan error
+	shutdownTimeout time.Duration
+}
+
+// New creates instance of new http server.
+func New(handler http.Handler, port string) *Server {
+	httpServer := &http.Server{
+		Addr:         ":" + port,
+		Handler:      handler,
+		ReadTimeout:  defaultReadTimeout,
+		WriteTimeout: defaultWriteTimeout,
+		IdleTimeout:  defaultIdleTimeout,
+	}
+
+	srv := &Server{
+		server:          httpServer,
+		notify:          make(chan error, 1),
+		shutdownTimeout: defaultShutdownTimeout,
+	}
+	srv.start()
+	return srv
+}
+
+// Start bootstraps http server.
+func (s *Server) start() {
+	log.Printf("Starting HTTP server on port %s", s.server.Addr)
+	go func() {
+		s.notify <- s.server.ListenAndServe()
+		close(s.notify)
+	}()
+}
+
+// Notify returns error notification channel.
+func (s *Server) Notify() <-chan error {
+	return s.notify
+}
+
+// Shutdown shuts down http server gracefully.
+func (s *Server) Shutdown() error {
+	ctx, cancel := context.WithTimeout(context.Background(), s.shutdownTimeout)
+	defer cancel()
+
+	return s.server.Shutdown(ctx)
+}
