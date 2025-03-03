8fc472041e46e46a801dd8ae285e5f7ed06055cd
chepaqq <chepaqq@protonmail.com>
2025-03-03 22:56:48 +0200

    feat: add jwt token to config

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,39 +4,43 @@ import (
 	"github.com/ilyakaznacheev/cleanenv"
 )
 
-type (
-	// Config holds various configuration settings
-	Config struct {
-		Server   HTTPConfig
-		Postgres PostgresConfig
-		Minio    MinioConfig
-	}
+// Config holds all configuration settings
+type Config struct {
+	Server   ServerConfig   `yaml:"server"`
+	Database DatabaseConfig `yaml:"database"`
+	Storage  StorageConfig  `yaml:"storage"`
+	Auth     AuthConfig     `yaml:"auth"`
+}
 
-	// HTTPConfig defines configuration settings for web server
-	HTTPConfig struct {
-		Port string `env:"PORT" env-default:"8000"`
-	}
+// ServerConfig defines configuration settings for the web server
+type ServerConfig struct {
+	Port string `env:"PORT" env-default:"8000"`
+}
 
-	// PostgresConfig defines configuration settings for PostgresConfig
-	PostgresConfig struct {
-		User     string `env:"POSTGRES_USER"     env-required:"true"`
-		Password string `env:"POSTGRES_PASSWORD" env-required:"true"`
-		Port     string `env:"POSTGRES_PORT"     env-required:"true"`
-		DBName   string `env:"POSTGRES_DB"       env-required:"true"`
-		Host     string `env:"POSTGRES_HOST"     env-required:"true"`
-		SSLMode  string `env:"POSTGRES_SSLMODE"  env-required:"true"`
-	}
+// DatabaseConfig defines configuration settings for Postgres
+type DatabaseConfig struct {
+	User     string `env:"POSTGRES_USER" env-required:"true"`
+	Password string `env:"POSTGRES_PASSWORD" env-required:"true"`
+	Port     string `env:"POSTGRES_PORT" env-required:"true"`
+	DBName   string `env:"POSTGRES_DB" env-required:"true"`
+	Host     string `env:"POSTGRES_HOST" env-required:"true"`
+	SSLMode  string `env:"POSTGRES_SSLMODE" env-required:"true"`
+}
 
-	// MinioConfig defines configuration settings for Minio
-	MinioConfig struct {
-		Endpoint       string `env:"MINIO_ENDPOINT"        env-required:"true"`
-		SSL            string `env:"MINIO_SSL_MODE"        env-required:"true"`
-		BucketName     string `env:"MINIO_BUCKET_NAME"     env-required:"true"`
-		BucketLocation string `env:"MINIO_BUCKET_LOCATION" env-required:"true"`
-	}
-)
+// StorageConfig defines configuration settings for MinIO
+type StorageConfig struct {
+	Endpoint       string `env:"MINIO_ENDPOINT" env-required:"true"`
+	SSL            bool   `env:"MINIO_SSL" env-default:"false"`
+	BucketName     string `env:"MINIO_BUCKET_NAME" env-required:"true"`
+	BucketLocation string `env:"MINIO_BUCKET_LOCATION" env-required:"true"`
+}
 
-// Init initialize app configuration
+// AuthConfig holds authentication-related settings
+type AuthConfig struct {
+	JWTSecret string `env:"JWT_SECRET" env-required:"true"`
+}
+
+// Init initializes the application configuration
 func Init() (*Config, error) {
 	var cfg Config
 	err := cleanenv.ReadConfig(".env", &cfg)
